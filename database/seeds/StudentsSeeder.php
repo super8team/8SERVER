b15655ad837edd7b5ac280606e7ccde215d0f13b
unknown <tjddnjs9903@naver.com>
2017-06-08 23:31:50 +0900

    StudentsSeeder modify

--- database/seeds/StudentsSeeder.php
+++ database/seeds/StudentsSeeder.php
@@ -11,63 +11,14 @@ class StudentsSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('students')->insert([
 
-            ['student' => DB::table('users')
-                ->where('type', 'student')->inRandomOrder()
-                ->first()->no, 'grade_class' => DB::table('grade_classes')
-                ->inRandomOrder()->first()->no, 'parents' => DB::table('users')
-                ->where('type', 'parents')->inRandomOrder()
-                ->first()->no],
-            ['student' => DB::table('users')
-                ->where('type', 'student')->inRandomOrder()
-                ->first()->no, 'grade_class' => DB::table('grade_classes')
-                ->inRandomOrder()->first()->no, 'parents' => DB::table('users')
-                ->where('type', 'parents')->inRandomOrder()
-                ->first()->no],
-            ['student' => DB::table('users')
-                ->where('type', 'student')->inRandomOrder()
-                ->first()->no, 'grade_class' => DB::table('grade_classes')
-                ->inRandomOrder()->first()->no, 'parents' => DB::table('users')
-                ->where('type', 'parents')->inRandomOrder()
-                ->first()->no],
-            ['student' => DB::table('users')
-                ->where('type', 'student')->inRandomOrder()
-                ->first()->no, 'grade_class' => DB::table('grade_classes')
-                ->inRandomOrder()->first()->no, 'parents' => DB::table('users')
-                ->where('type', 'parents')->inRandomOrder()
-                ->first()->no],
-            ['student' => DB::table('users')
-                ->where('type', 'student')->inRandomOrder()
-                ->first()->no, 'grade_class' => DB::table('grade_classes')
-                ->inRandomOrder()->first()->no, 'parents' => DB::table('users')
-                ->where('type', 'parents')->inRandomOrder()
-                ->first()->no],
-            ['student' => DB::table('users')
-                ->where('type', 'student')->inRandomOrder()
-                ->first()->no, 'grade_class' => DB::table('grade_classes')
-                ->inRandomOrder()->first()->no, 'parents' => DB::table('users')
-                ->where('type', 'parents')->inRandomOrder()
-                ->first()->no],
-            ['student' => DB::table('users')
-                ->where('type', 'student')->inRandomOrder()
-                ->first()->no, 'grade_class' => DB::table('grade_classes')
-                ->inRandomOrder()->first()->no, 'parents' => DB::table('users')
-                ->where('type', 'parents')->inRandomOrder()
-                ->first()->no],
-            ['student' => DB::table('users')
-                ->where('type', 'student')->inRandomOrder()
-                ->first()->no, 'grade_class' => DB::table('grade_classes')
-                ->inRandomOrder()->first()->no, 'parents' => DB::table('users')
-                ->where('type', 'parents')->inRandomOrder()
-                ->first()->no],
-            ['student' => DB::table('users')
-                ->where('type', 'student')->inRandomOrder()
-                ->first()->no, 'grade_class' => DB::table('grade_classes')
-                ->inRandomOrder()->first()->no, 'parents' => DB::table('users')
-                ->where('type', 'parents')->inRandomOrder()
-                ->first()->no]
-
-        ]);
+        $students = DB::table('users')->where('type', 'student')->get();
+        foreach ($students as $student) {
+            DB::table('students')->insert([
+                ['student'=>$student->no,
+                 'grade_class'=>DB::table('grade_classes')->inRandomOrder()->first()->no,
+                 'parents'=>DB::table('users')->where('type', 'parents')->inRandomOrder()->first()->no],
+            ]);
+        }
     }
 }
