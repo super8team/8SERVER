f4a6652961f08bf3cd1978fdeb644f85610efb4a
unknown <tjddnjs9903@naver.com>
2017-06-13 12:51:39 +0900

    update_history_substances_migration

---
+++ database/migrations/2017_06_13_034314_update_history_substances_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateHistorySubstancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('history_substances', function (Blueprint $table) {
+            $table->renameColumn('wheather', 'weather');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('history_substances', function (Blueprint $table) {
+            $table->string('wheather', 255)->change();
+        });
+    }
+}
