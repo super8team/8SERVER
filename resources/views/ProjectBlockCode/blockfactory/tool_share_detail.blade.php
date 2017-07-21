c7f1acefa6de0c1c0c3280ccd8053c83346e1e14
unknown <>
2017-07-22 00:04:10 +0900

    package average 2

--- resources/views/ProjectBlockCode/blockfactory/tool_share_detail.blade.php
+++ resources/views/ProjectBlockCode/blockfactory/tool_share_detail.blade.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="{{URL::asset('/css/factory.css')}}">
-
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
 <body>
   <span id = "content_wrapper">
     <div>
@@ -57,6 +57,87 @@
   <!-- <button id="content_download" type="button" name="button">다운로드</button> -->
 
   <script type="text/javascript">
+  function myfunction(){
+              var s = prompt('점수를 입력','점수');
+              console.log(s);
+              switch(s){
+                case '1':
+
+                  document.getElementsByName('star-input')[0].checked=true;
+                break;
+                case '2':
+
+                  document.getElementsByName('star-input')[1].checked=true;
+                break;
+                case '3':
+
+                  document.getElementsByName('star-input')[2].checked=true;
+                break;
+                case '4':
+
+                  document.getElementsByName('star-input')[3].checked=true;
+                break;
+                case '5':
+
+                  document.getElementsByName('star-input')[4].checked=true;
+                break;
+                case '6':
+
+                  document.getElementsByName('star-input')[5].checked=true;
+                break;
+                case '7':
+
+                  document.getElementsByName('star-input')[6].checked=true;
+                break;
+                case '8':
+
+                  document.getElementsByName('star-input')[7].checked=true;
+                break;
+                case '9':
+
+                  document.getElementsByName('star-input')[8].checked=true;
+                break;
+                case '10':
+
+                  document.getElementsByName('star-input')[8].checked=true;
+                break;
+
+              }
+            }
+            var starRating = function(){
+            var $star = $(".star-input"),
+                $result = $star.find("output>b");
+            $(document)
+              .on("focusin", ".star-input>.input", function(){
+              $(this).addClass("focus");
+            })
+              .on("focusout", ".star-input>.input", function(){
+              var $this = $(this);
+              setTimeout(function(){
+                console.log('1');
+                if($this.find(":focus").length === 0){
+                  $this.removeClass("focus");
+                }
+              }, 100);
+            })
+              .on("change", ".star-input :radio", function(){
+              $result.text($(this).next().text());
+            })
+              .on("mouseover", ".star-input label", function(){
+                console.log('2');
+              $result.text($(this).text());
+            })
+              .on("mouseleave", ".star-input>.input", function(){
+              var $checked = $star.find(":checked");
+              console.log($checked);
+              if($checked.length === 0){
+                $result.text("0");
+              } else {
+                $result.text($checked.next().text());
+              }
+            });
+            };
+            starRating();
   $(document).ready(function(){
     document.getElementById('content_download').addEventListener('click',function(){
       var package_num = document.getElementsByName('package_num')[0].value;
