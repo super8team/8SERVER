a89577a006f931b7aff86922a5663ffd188e47a8
kwonyuseong <kwonyuseong94@gmail.com>
2017-07-31 11:01:32 +0900

    AppRequestController 409 line

--- app/Http/Controllers/AppRequestController.php
+++ app/Http/Controllers/AppRequestController.php
@@ -404,7 +404,7 @@ class AppRequestController extends Controller
       return json_encode([
         "content" => $content,
         "input-score" => $score,
-        "avg-score" => $avgScore
-      ])
+        "avg-score" => $avgScore,
+      ]);
     }
 }
