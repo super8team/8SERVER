1f4352c69cf5e2207ff96d85b8039d113b8bfe18
unknown <>
2017-08-07 12:32:37 +0900

    modify image url modify

--- resources/views/ProjectBlockCode/blockfactory/tool_share_detail.blade.php
+++ resources/views/ProjectBlockCode/blockfactory/tool_share_detail.blade.php
@@ -10,7 +10,8 @@
       @php
         $url = Storage::url('packageImgs/'.$package_img);
       @endphp
-      <img src="http://163.44.166.91/LEARnFUN/public/storage/{{$url}}" alt="콘텐츠 이미지" class="content_img">
+                http://163.44.166.91/LEARnFUN/public/storage/packageImgs/image2.jpg
+      <img src="http://163.44.166.91/LEARnFUN/public/storage/{{$url}}" alt="{{$url}}" class="content_img">
       <div style="display:inline">
         <span class="star-input" style="margin-top:170px; margin-left:100px;">
           <span class="input" style="margin-top:50px">
