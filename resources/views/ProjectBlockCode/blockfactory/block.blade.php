6fb5b1afb8d64edfed7505b2e9e63bbc2c6af94a
unknown <>
2017-08-21 20:44:01 +0900

    main view show contents

--- resources/views/ProjectBlockCode/blockfactory/block.blade.php
+++ resources/views/ProjectBlockCode/blockfactory/block.blade.php
@@ -352,7 +352,15 @@
                       </div>
                       <!-- 패키지 리스트 출력 코드 -->
                         @if($packages)
-                        
+                            @for($i=0; $i < 8; $i++)
+                            <button style="margin-bottom:35px;margin-left:15px;height:50px" class="content_list" type="button" name="button" value="{{$first_package[$i]['xml']}}" >
+                               {{$first_package[$i]['name']}}
+                               <input type="text" class="contents_xml"  value="{{$first_package[$i]['xml']}}" hidden>
+                               <input type="text" class="block_myungse" value="{{$first_package[$i]['spec']}}" hidden>
+                               <input type="text" name="id"  value="{{$first_package[$i]['id']}}" hidden>
+                           </button>
+                           <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                           @endfor
                        @endif
                    </div>
                     </form>
