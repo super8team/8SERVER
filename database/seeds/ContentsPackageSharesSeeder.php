576c7fc8d98d4ee4c4696892ddae55ffe19c6b50
root <kimill1394@gmail.com>
2017-08-25 00:44:19 +0900

    z

--- database/seeds/ContentsPackageSharesSeeder.php
+++ database/seeds/ContentsPackageSharesSeeder.php
@@ -14,15 +14,15 @@ class ContentsPackageSharesSeeder extends Seeder
        //
 
         DB::table('contents_package_shares')->insert([
-            ['contents_package'=>1,'img_url'=>'image1.jpg','explain'=>'첨성대, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'12','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['contents_package'=>2,'img_url'=>'image2.jpg','explain'=>'영진, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'23','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['contents_package'=>3,'img_url'=>'image3.jpg','explain'=>'김종률교수님, 이중권교수님, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'43','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['contents_package'=>5,'img_url'=>'image4.jpg','explain'=>'야구장, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'1', 'downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['contents_package'=>6,'img_url'=>'image5.jpg','explain'=>'경주, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'23','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['contents_package'=>7,'img_url'=>'image6.jpg','explain'=>'서울, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'54','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['contents_package'=>8,'img_url'=>'image7.jpg','explain'=>'3층석탑, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'45','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['contents_package'=>9,'img_url'=>'image8.jpg','explain'=>'첨성대, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'45','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['contents_package'=>8,'img_url'=>'image9.jpg','explain'=>'첨성대, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'64','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['contents_package'=>1,'img_url'=>'image1.jpg','explain'=>'첨성대, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'12','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),'package_name'=>'불국사'],
+           ['contents_package'=>2,'img_url'=>'image2.jpg','explain'=>'영진, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'23','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),'package_name'=>'릉의 역사'],
+           ['contents_package'=>3,'img_url'=>'image3.jpg','explain'=>'김종률교수님, 이중권교수님, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'43','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),'package_name'=>'불국사'],
+            ['contents_package'=>5,'img_url'=>'image4.jpg','explain'=>'야구장, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'1', 'downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),'package_name'=>'향교'],
+            ['contents_package'=>6,'img_url'=>'image5.jpg','explain'=>'경주, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'23','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),'package_name'=>'수원 화성'],
+            ['contents_package'=>7,'img_url'=>'image6.jpg','explain'=>'서울, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'54','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),'package_name'=>'첨성대'],
+            ['contents_package'=>8,'img_url'=>'image7.jpg','explain'=>'3층석탑, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'45','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),'package_name'=>'웅과 환'],
+            ['contents_package'=>9,'img_url'=>'image8.jpg','explain'=>'첨성대, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'45','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),'package_name'=>'6.25'],
+            ['contents_package'=>8,'img_url'=>'image9.jpg','explain'=>'첨성대, 불국사, 왕릉의 역사를 공부할 수 있는 문제!','views'=>'64','downloads'=>4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),'package_name'=>'민주화운동'],
 
         ]);
     }
