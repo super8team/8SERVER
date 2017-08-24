9985078bba5dc4bf4c179479b4a04a9bc4c38492
unknown <>
2017-08-24 09:12:00 +0900

    contents

--- app/Http/Controllers/ContentsController.php
+++ app/Http/Controllers/ContentsController.php
@@ -21,9 +21,9 @@ class ContentsController extends Controller
           $packages = [];
           // $owndedPackages = \DB::table('contents_packages')->where('owner', $userNo)->get();
           $owndedPackages = \DB::table('contents_packages')->where('owner', $userNo)->get();
-      
 
-          if($owndedPackaged->isEmpty())){
+
+          if($owndedPackaged->isEmpty()){
             return view('ProjectBlockCode.blockfactory.block', ['packages' => null,'contentsize'=>null,'index'=>0,'user'=>Auth::user()->name]);
           }else{
             $content_count = DB::table('contents')->where('contents_package',$owndedPackages[0]->no)->get();
