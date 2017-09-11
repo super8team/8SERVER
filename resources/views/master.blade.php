ac8f38346bd44247dcb80f62435cb32c41fadb4c
kwonyuseong <kwonyuseong94@gmail.com>
2017-09-11 17:31:57 +0900

    ui fixing

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -19,12 +19,12 @@
 
     <!-- 부트스트랩 -->
     {{-- <link href="../public/css/bootstrap.css" rel="stylesheet"> --}}
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css?=4') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css?=5') }}">
     {{-- <link rel="stylesheet" href="{{ asset('css/boottheme.css?ver=3') }}"> --}}
     {{-- 커스텀 css --}}
     {{-- <link rel="stylesheet" href="../public/css/custom.css"> --}}
-    <link rel="stylesheet" href="{{ asset('css/custom.css?ver=5') }}">
-    <link href="https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('css/custom.css?ver=6') }}">
+    <link href="https://fonts.googleapis.com/earlyaccess/sawarabigothic.css" rel="stylesheet" />
 
     <script src="{{ asset('js/parallax.js') }}"></script>
     <configuration>
