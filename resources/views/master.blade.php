2bc6701f9b67e3195806bca34f553a2e97d6452a
kwonyuseong <kwonyuseong94@gmail.com>
2017-09-11 12:22:57 +0900

    ui fixing now

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -84,7 +84,7 @@
   <body>
     {{-- 메인 --}}
     <!-- Modal 모달 -->
-    <div class="modal " id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal fade " id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
