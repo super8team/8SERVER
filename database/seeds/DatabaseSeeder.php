c5f51c5a2ca32f0e8171488b17e789e108ef3add
kimill1394 <kimill1394@gmail.com>
2017-09-27 11:24:16 +0900

    update database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 <?php
-=======
-00<?php
->>>>>>> afee925e9a3fcad0f1b5dd50e987eac66e0a568d
 
 use Illuminate\Database\Seeder;
 
@@ -15,52 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        //
-	$this->call(ContentsPackageSharesSeeder::class);
-    }
-=======
-    //  $this->call(UserSeeder::class);
-    //  $this->call(FieldLearningPlanStepsSeeder::class);
-    //  $this->call(EtcSelectArticlesSeeder::class);
-    //  $this->call(TrafficArticlesSeeder::class);
-    // $this->call(SchoolsSeeder::class);
-    //  $this->call(PlaceSeeder::class);
-    // $this->call(FieldLearningProgramArticlesSeeder::class);
-    // $this->call(InstAuthArticlesSeeder::class);
-    // $this->call(FieldLearningPlanWorksSeeder::class);
-    // $this->call(FieldLearningPlanDocumentsSeeder::class);
-    // $this->call(StudentsSeeder::class);
-//    $this->call(CheckListsSeeder::class);
-    // $this->call(FieldLearningPlansSeeder::class);
-
-        // $this->call(DetailPlansSeeder::class);
-        // $this->call(DetailPlanShareSeeder::class);
-     $this->call(GroupsSeeder::class);
-
-//      $this->call(ContentsPackageSeeder::class);
-        // $this->call(LogSeeder::class);
-//      $this->call(HistoriesSeeder::class);
-//      $this->call(HistorySubstancesSeeder::class);
-//      $this->call(HistoryImgsSeeder::class);
-//      $this->call(WorkProgressesSeeder::class);
-//      $this->call(SurveySeeder::class);
-//      $this->call(SurveyArticlesSeeder::class);
-//      $this->call(SurveyAnswersSeeder::class);
-    //  $this->call(PlanChecklistsSeeder::class);
-
-        // $this->call(ContentsSeeder::class);
-    //     $this->call(ContentsPackageSharesSeeder::class);
-
-//        $this->call(PlanDemoSeeder::class);
-
-        // $this->call(KoreanUserSeeder::class);
-        // $this->call(ContentsSeeder::class);
-        // $this->call(ContentsSeeder::class);
-
-
-        // $this->call(NoticeSeeder::class);
+        $this->call(JapaneseUserSeeder::class);
     }
 
->>>>>>> afee925e9a3fcad0f1b5dd50e987eac66e0a568d
 }
